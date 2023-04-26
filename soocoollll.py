7cf7dfc277f4cafed0c563e19c91afc6b6856d64
V3rmillionCreator <104515059+V3rmillionCreator@users.noreply.github.com>
2023-04-26 11:45:59 -0500

    Add files via upload

---
+++ soocoollll.py
@@ -0,0 +1,82 @@
+import time
+print("PyShell is loading")
+time.sleep(4)
+print("Checking directory for files")
+time.sleep(2)
+print("FIles checked:")
+print("0 Thread-1 worker thread checking in")
+time.sleep(1)
+print("1 Thread-2 worker thread checking in")
+print("1 MainThread Checking in from main thread")
+print("752 MainThread Checking in from main thread")
+print("1001 Thread-1 worker thread checking in")
+time.sleep(0.3)
+print("1001 Thread-2 worker thread checking in")
+time.sleep(0.1)
+print("1502 MainThread Checking in from main thread")
+time.sleep(0.1)
+print("2003 Thread-1 worker thread checking in")
+time.sleep(0.1)
+print("2003 Thread-2 worker thread checking in")
+print("2253 MainThread Checking in from main thread")
+print("3005 Thread-1 worker thread checking in")
+time.sleep(2)
+print("3005 MainThread Checking in from main thread")
+print("3005 Thread-2 worker thread checking in")
+time.sleep(2)
+print("All files are present. Finishing up.")
+time.sleep(4)
+print("Checking comparison Repo(Git)")
+time.sleep(9)
+print("all checks out! console will now be available")
+x = """
+
+██████████████████████████████████████████████████████████████████████████████████████████████████████████████
+█░░░░░░░░░░░░░░█░░░░░░░░██░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░██░░░░░░█░░░░░░░░░░░░░░█░░░░░░█████████░░░░░░█████████
+█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀░░██░░▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░█████████░░▄▀░░█████████
+█░░▄▀░░░░░░▄▀░░█░░░░▄▀░░██░░▄▀░░░░█░░▄▀░░░░░░░░░░█░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░█████████░░▄▀░░█████████
+█░░▄▀░░██░░▄▀░░███░░▄▀▄▀░░▄▀▄▀░░███░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░█████████░░▄▀░░█████████░░▄▀░░█████████
+█░░▄▀░░░░░░▄▀░░███░░░░▄▀▄▀▄▀░░░░███░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░█████████░░▄▀░░█████████
+█░░▄▀▄▀▄▀▄▀▄▀░░█████░░░░▄▀░░░░█████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░█████████░░▄▀░░█████████
+█░░▄▀░░░░░░░░░░███████░░▄▀░░███████░░░░░░░░░░▄▀░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░█████████░░▄▀░░█████████
+█░░▄▀░░███████████████░░▄▀░░███████████████░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░█████████░░▄▀░░█████████░░▄▀░░█████████
+█░░▄▀░░███████████████░░▄▀░░███████░░░░░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░░░█
+█░░▄▀░░███████████████░░▄▀░░███████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█
+█░░░░░░███████████████░░░░░░███████░░░░░░░░░░░░░░█░░░░░░██░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█
+██████████████████████████████████████████████████████████████████████████████████████████████████████████████
+"""
+time.sleep(2)
+print(x)
+send_request = [
+        "Who would you like to send a chat request to?",
+    ]
+info = [
+        "PyShell is a experimental CLI (Command Line Interface) that uses python!",
+    ]
+Website = [
+        "Friendai.my.canva.site/test",
+    ]
+source = [
+        "Sorry, this version of PyShell is SkidProof!",
+    ]
+Import = [
+        "Syntax Error: Did you mean, import (module)?",
+    ]
+import_os = [
+        "The module: OS is already imported!",
+    ]
+import_time = [
+        "The module: time is already imported!",
+    ]
+import_pystyle = [
+        "Import failed. Error 406: Syntax Error, >>pystyle.color<<",
+    ] 
+teacher = [
+        "Sending chat request to @Teacher Role.",
+    ]
+commands_help = [
+        "Import, send_request, source, Website, info",
+    ] 
+pyshell_version = [
+        "v0.0.3",
+    ]
